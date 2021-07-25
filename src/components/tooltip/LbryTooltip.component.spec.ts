56c0541a7c1da5af2348ec3f39dcbb48028b0c1f
MicheleLiberio <mikelelib@libero.it>
2021-07-25 21:22:01 +0200

    tooltip

--- src/components/tooltip/LbryTooltip.component.spec.ts
+++ src/components/tooltip/LbryTooltip.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { TooltipComponent } from './tooltip.component';
+import { TooltipComponent } from './LbryTooltip.component';
 
 describe('TooltipComponent', () => {
   let component: TooltipComponent;
--- src/components/tooltip/LbryTooltip.component.ts
+++ src/components/tooltip/LbryTooltip.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tooltip',
-  templateUrl: './tooltip.component.html',
-  styleUrls: ['./tooltip.component.scss']
+  selector: 'lbry-tooltip',
+  templateUrl: './LbryTooltip.component.html',
+  styleUrls: ['./LbryTooltip.component.scss']
 })
-export class TooltipComponent implements OnInit {
+export class LbryTooltipComponent implements OnInit {
 
   @Input() message: string;
   @Input() icon: string;
