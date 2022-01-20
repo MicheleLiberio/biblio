4241a0f85dee9854394f808cafbb519d2991c079
MicheleLiberio <mikelelib@libero.it>
2022-01-20 22:30:41 +0100

    commit section book

---
+++ src/app/borrow/search-book/section-book/section-book.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { SectionBookComponent } from './section-book.component';
+
+describe('SectionBookComponent', () => {
+  let component: SectionBookComponent;
+  let fixture: ComponentFixture<SectionBookComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SectionBookComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SectionBookComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
