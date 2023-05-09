3302489b4e2ad88d9fab660a687a7baaa4776ec8
MicheleLiberio <mikelelib@libero.it>
2023-05-09 21:44:23 +0200

    commit interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule, routingComponents, components } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { PagesComponent } from './pages/pages.component';
 import { FooterComponent } from './footer/footer.component';
+import { LoadingInterceptor } from './loading.Interceptor';
 
 @NgModule({
   declarations: [				
@@ -24,7 +25,13 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide: HTTP_INTERCEPTORS,
+    useClass: LoadingInterceptor,
+    multi: true,
+  },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
