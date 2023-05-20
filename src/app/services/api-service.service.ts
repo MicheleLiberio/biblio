66e166e204d913576e767f9dcde7b896ea172140
MicheleLiberio <mikelelib@libero.it>
2023-05-20 17:28:54 +0200

    commit tables

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,34 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { User } from '../class/user/user';
-import { Lend } from '../class/lend/lend';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../class/user/user";
+import { Lend } from "../class/lend/lend";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ApiService {
+  private url = "http://localhost:8083/api";
 
-private url = 'http://localhost:8083/api';
+  constructor(private httpClient: HttpClient) {}
 
+  getBooks(searchKey: String) {
+    return this.httpClient.get(
+      `${this.url}/books?searchKey=${searchKey ? searchKey : ""}`
+    );
+  }
 
-constructor(private httpClient: HttpClient) { }
-  
-getBooks(searchKey: String) {
-  return this.httpClient.get(`${this.url}/books?searchKey=${searchKey ? searchKey : ''}`);
-}
+  getUsers(nameSearch: string, surnameSearch: string) {
+    return this.httpClient.get(
+      `${this.url}/readers?name=${nameSearch ? nameSearch : ""}&surname=${
+        surnameSearch ? surnameSearch : ""
+      }`
+    );
+  }
 
-getUsers(nameSearch: string, surnameSearch: string) {
-  return this.httpClient.get(`${this.url}'/readers?name='${nameSearch ? nameSearch : ''}'&surname='${surnameSearch ? surnameSearch : ''}`);
-}
+  setUser(user: User) {
+    console.log("user: ", user);
+    return this.httpClient.post(`${this.url}/readers`, user);
+  }
 
-setUser(user: User) {
-  console.log("user: ", user);
-  return this.httpClient.post(`${this.url}'/readers`, user)
-}
-
-setLend(lend: Lend) {
-  console.log("lend: ", lend);
-  return this.httpClient.post(`${this.url}'/lends`, lend)
-}
-
+  setLend(lend: Lend) {
+    console.log("lend: ", lend);
+    return this.httpClient.post(`${this.url}'/lends`, lend);
+  }
 }
