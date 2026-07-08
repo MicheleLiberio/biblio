456afff5a5bebeb5ffab17685b5d87e4439cd6ef
MicheleLiberio <50373481+MicheleLiberio@users.noreply.github.com>
2026-07-08 22:17:29 +0200

    Update modal-basic.ts

--- src/app/modal-basic/modal-basic.ts
+++ src/app/modal-basic/modal-basic.ts
@@ -8,6 +8,7 @@ import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 })
 export class NgbdModalBasiccc {
   closeResult: string;
+  content: any;
 
   constructor(private modalService: NgbModal) {}
 
