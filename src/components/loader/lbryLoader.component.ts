6b87e56e2817fafecea0a958945cef61f97476e8
MicheleLiberio <50373481+MicheleLiberio@users.noreply.github.com>
2026-07-08 22:02:48 +0200

    Update lbryLoader.component.ts

--- src/components/loader/lbryLoader.component.ts
+++ src/components/loader/lbryLoader.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lbry-loader',
-  templateUrl: './LbryLoader.component.html',
+  templateUrl: './lbryLoader.component.html',
   styleUrls: ['./lbryLoader.component.scss']
 })
 export class LbryLoaderComponent implements OnInit {
